1860c791ea39a8fe04ddd26b9d883aec19b11059
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-02-14 17:07:26 +0200

    add functionality for merging a tile with another one

--- src/js/Cell.js
+++ src/js/Cell.js
@@ -5,6 +5,7 @@ export default class Cell {
     this.row = row;
     this.col = col;
     this.tile = null;
+    this.mergeTile = null;
   }
 
   isEmpty() {
@@ -24,8 +25,34 @@ export default class Cell {
     return this.tile;
   }
 
+  setMergeTile(tile) {
+    if (this.mergeTile) {
+      throw new Error("cell already has a merge tile");
+    }
+    this.mergeTile = tile;
+  }
+
   clearTile() {
     this.tile = null;
+  }
+
+  clearMergeTile() {
+    this.mergeTile = null;
+  }
+
+  canMerge(tile) {
+    if (!this.tile) {
+      throw new Error("cell has no tile");
+    }
+    return !this.mergeTile && (this.tile.getValue() == tile.getValue());
+  }
+
+  mergeTiles() {
+    if (!this.tile || !this.mergeTile) {
+      return;
+    }
+    this.tile.setValue(this.tile.getValue() + this.mergeTile.getValue());
+    this.clearMergeTile();
   }
 
   setNewTile() {
