85f4e79849fb0a5719a837544c37af3949cfa449
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-02-14 17:10:37 +0200

    add functionality for printing a board

--- src/js/Grid.js
+++ src/js/Grid.js
@@ -14,4 +14,38 @@ export default class Grid {
     this.cols = this.rows.map((_, i) => this.rows.map(row => row[i]));
     this.changed = false;
   }
+
+  findMaxValue() {
+    let max = 0;
+    for (let i = 0; i < this.rows.length; i++) {
+      for (let j = 0; j < this.cols.length; j++) {
+        const cell = this.rows[i][j];
+        if (!cell.isEmpty() && cell.getTile().getValue() > max) {
+          max = cell.getTile().getValue();
+        }
+      }
+    }
+    return max;
+  }
+
+  toString() {
+    let result = '';
+    const maxDigits = this.findMaxValue().toString().length;
+    const pad = ' '.repeat(maxDigits);
+    for (let i = 0; i < this.rows.length; i++) {
+      for (let j = 0; j < this.cols.length; j++) {
+        const cell = this.rows[i][j];
+        if (cell.isEmpty()) {
+          result += (pad + '.').slice(-maxDigits);
+        } else {
+          result += (pad + cell.getTile().getValue()).slice(-maxDigits);
+        }
+        if (j != this.rows.length - 1) {
+          result += ' ';
+        }
+      }
+      result += '\n';
+    }
+    return result;
+  }
 }
