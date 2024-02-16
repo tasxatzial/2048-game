2a7830f19853162c23533057df0b7c172da4262c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-02-16 18:31:46 +0200

    we are now able to play the game in the console

---
+++ src/js/Game.js
@@ -0,0 +1,79 @@
+import Grid from "./Grid.js";
+
+export default class Game {
+  constructor() {
+    this.grid = new Grid({
+      size: 4,
+      newTileFn: this.newTileFn,
+      mergeResultFn: this.mergeResultFn
+    });
+
+    this.grid.addTile();
+    this.grid.addTile();
+  }
+  
+  printBoard() {
+    return this.grid.toString();
+  }
+
+  isWon() {
+    return this.grid.hasTile(2048);
+  }
+
+  isLost() {
+    return !this.grid.canSlide();
+  }
+
+  hasSlid() {
+    return this.grid.hasChangedAfterSlide();
+  }
+
+  getScore() {
+    return null;
+  }
+
+  _postSlide() {
+    if (this.grid.hasChangedAfterSlide()) {
+      this.grid.mergeTiles();
+      this.grid.addTile();
+    }
+  }
+
+  slideLeft() {
+    this.grid.slideLeft();
+    this._postSlide();
+  }
+
+  slideRight() {
+    this.grid.slideRight();
+    this._postSlide();
+  }
+
+  slideUp() {
+    this.grid.slideUp();
+    this._postSlide();
+  }
+
+  slideDown() {
+    this.grid.slideDown();
+    this._postSlide();
+  }
+
+  newTileFn(gridArray) {
+    const emptyTiles = gridArray.map(row => row.filter(el => !el.value)).flat();
+    if (emptyTiles.length == 0) {
+      return null;
+    }
+    const randEl = emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
+    const value = Math.floor(Math.random() > 0.9 ? 4 : 2);
+    return {
+      row: randEl.row,
+      column: randEl.column,
+      value: value
+    };
+  }
+  
+  mergeResultFn(value1, value2) {
+    return value1 + value2;
+  }
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,36 @@
-import demo from './demo.js';
+import Game from "./Game.js";
 
-demo();
+const game = new Game();
+console.log(game.printBoard())
+
+function keydownHandler(e) {
+  if (e.repeat) {
+    return;
+  }
+  switch(e.code) {
+    case 'ArrowLeft':
+      game.slideLeft();
+      break;
+    case 'ArrowUp':
+      game.slideUp();
+      break;
+    case 'ArrowRight':
+      game.slideRight();
+      break;
+    case 'ArrowDown':
+      game.slideDown();
+      break;
+  }
+  if (game.hasSlid()) {
+    //console.log("Score ", game.getScore());
+    console.log(game.printBoard());
+    if (game.isWon()) {
+      console.log("You won!");
+    }
+    else if (game.isLost()) {
+      console.log("No more moves");
+    }
+  }
+}
+
+window.addEventListener('keydown', keydownHandler);
