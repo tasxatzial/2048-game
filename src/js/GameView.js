afed9bf4717de239b5ce5e329128cbca97f66084
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-05-20 16:23:32 +0300

    we can now slide the tiles with mouse events

--- src/js/GameView.js
+++ src/js/GameView.js
@@ -1,13 +1,21 @@
 import BoardView from "./gameView/BoardView.js";
 
 export default class GameView {
-  constructor() {
+  constructor(obj) {
+    if (!obj) {
+      obj = {};
+    }
     this.boardView = new BoardView();
     this.score = document.querySelector('.js-current-score');
     this._onKeydown = this._onKeydown.bind(this);
+    this._onMouseDown = this._onMouseDown.bind(this);
+    this._onMouseMove = this._onMouseMove.bind(this);
+    this._onMouseUp = this._onMouseUp.bind(this);
     this.slideHandlers = null;
     this.slidePermitted = false;
     this.grid = null;
+    this.mouseDownPos = null;
+    this.mouseTriggerDistance = obj.mouseTriggerDistance || 40;
   }
 
   initialize(game) {
@@ -37,11 +45,16 @@ export default class GameView {
   bindHandlers(slideHandlers) {
     this.slideHandlers = slideHandlers;
     window.addEventListener('keydown', this._onKeydown);
+    this.grid.addEventListener('mousedown', this._onMouseDown);
+    document.addEventListener('mouseup', this._onMouseUp);
+    document.addEventListener('mousemove', this._onMouseMove);
   }
 
   removeHandlers() {
     this.slideHandlers = null;
     window.removeEventListener('keydown', this._onKeydown);
+    document.removeEventListener('mouseup', this._onMouseUp);
+    document.removeEventListener('mousemove', this._onMouseMove);
   }
 
   updateGameStatus(game) {
@@ -89,5 +102,41 @@ export default class GameView {
           break;
       }
     }
+  }
+
+  _onMouseDown(e) {
+    this.mouseDownPos = {
+      x: e.clientX,
+      y: e.clientY
+    }
+  }
+
+  _onMouseUp(e) {
+    this.mouseDownPos = null;
+  }
+
+  _onMouseMove(e) {
+    if (!this.mouseDownPos || !this.slidePermitted) {
+      return;
+    }
+    const horizontalDistance = e.clientX - this.mouseDownPos.x;
+    const verticalDistance = e.clientY - this.mouseDownPos.y;
+    if (Math.abs(horizontalDistance) > this.mouseTriggerDistance ||
+        Math.abs(verticalDistance) > this.mouseTriggerDistance) {
+          this.slidePermitted = false;
+          this.mouseDownPos = null;
+          if (horizontalDistance > this.mouseTriggerDistance) {
+            this.slideHandlers.slideRight();
+          }
+          else if (horizontalDistance < -this.mouseTriggerDistance) {
+            this.slideHandlers.slideLeft();
+          }
+          else if (verticalDistance > this.mouseTriggerDistance) {
+            this.slideHandlers.slideDown();
+          }
+          else if (verticalDistance < -this.mouseTriggerDistance) {
+            this.slideHandlers.slideUp();
+          }
+        }
   }
 }
