7beceeb253444107f41503521fc680e96a7794ee
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-06-17 13:03:08 +0300

    explain function parameters in merge condition function

--- src/js/gameModel/MergeCondition.js
+++ src/js/gameModel/MergeCondition.js
@@ -1,6 +1,7 @@
 export default class MergeCondition {
   constructor() {}
 
+  //first parameter corresponds to the tile closest to the edge in the slide direction
   static original2048(val1, val2) {
     return val1 === val2;
   }
