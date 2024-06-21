14a9fb062f230f451d7a2599b7f4dbe8d0004e0b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-06-21 11:10:56 +0300

    update the color representation in the array of tile colors

--- src/js/colors.js
+++ src/js/colors.js
@@ -3,6 +3,6 @@ import { COLORS } from './gameView/BoardViewColorModel.js';
 const tiles = document.querySelectorAll('.tile');
 
 for (let i = 0; i < tiles.length; i++) {
-  tiles[i].style.color = COLORS[i][0];
-  tiles[i].style.backgroundColor = COLORS[i][1];
+  tiles[i].style.color = COLORS[i].color;
+  tiles[i].style.backgroundColor = COLORS[i].backgroundColor;
 }
--- src/js/gameView/BoardViewColorModel.js
+++ src/js/gameView/BoardViewColorModel.js
@@ -2,13 +2,13 @@ export default class BoardViewColorModel {
   constructor() {
     this.tileColors = new Map();
     this.colors = [];
-    for (let i = 0; i < COLORS.length; i++) {
+    COLORS.forEach(({color, backgroundColor}) => {
       this.colors.push({
-        'color': COLORS[i][0],
-        'backgroundColor': COLORS[i][1],
+        color,
+        backgroundColor,
         'used': false
       });
-    }
+    });
   }
 
   getTileColor(tileValue) {
@@ -61,42 +61,43 @@ export default class BoardViewColorModel {
 }
 
 export const COLORS = [
-  ['black', 'hsl(50, 77%, 75%)'],
-  ['black', 'hsl(25, 88%, 74%)'],
-  ['black', 'hsl(120, 87%, 78%)'],
-  ['black', 'hsl(180, 85%, 70%)'],
-  ['black', 'hsl(60, 80%, 50%)'],
-  ['black', 'hsl(39, 90%, 45%)'],
-  ['black', 'hsl(0, 71%, 69%)'],
-  ['black', 'hsl(150, 88%, 44%)'],
-  ['black', 'hsl(195, 53%, 79%)'],
-  ['black', 'hsl(302, 55%, 70%)'],
-  ['black', 'hsl(0, 29%, 72%)'],
-  ['black', 'hsl(20, 88%, 60%)'],
-  ['black', 'hsl(95, 76%, 56%)'],
-  ['black', 'hsl(49, 84%, 45%)'],
-  ['black', 'hsl(195, 100%, 60%)'],
-  ['black', 'hsl(115, 20%, 60%)'],
-  ['black', 'hsl(256, 62%, 76%)'],
-  ['black', 'hsl(300, 81%, 70%)'],
-  ['black', 'hsl(39, 91%, 57%)'],
-  ['black', 'hsl(0, 4%, 68%)'],
-  ['white', 'hsl(0, 68%, 42%)'],
-  ['white', 'hsl(120, 61%, 34%)'],
-  ['white', 'hsl(210, 62%, 54%)'],
-  ['white', 'hsl(271, 76%, 53%)'],
-  ['white', 'hsl(240, 92%, 59%)'],
-  ['white', 'hsl(60, 89%, 22%)'],
-  ['white', 'hsl(0, 77%, 60%)'],
-  ['white', 'hsl(25, 76%, 31%)'],
-  ['white', 'hsl(328, 66%, 35%)'],
-  ['white', 'hsl(249, 100%, 70%)'],
-  ['white', 'hsl(80, 65%, 35%)'],
-  ['white', 'hsl(180, 100%, 27%)'],
-  ['white', 'hsl(210, 13%, 50%)'],
-  ['white', 'hsl(338, 47%, 52%)'],
-  ['white', 'hsl(125, 73%, 20%)'],
-  ['white', 'hsl(18, 91%, 48%)'],
-  ['white', 'hsl(220, 89%, 28%)'],
-  ['white', 'hsl(312, 80%, 42%)'],
-  ['black', 'white']];
+  {id:1,  color: 'black', backgroundColor: 'hsl(50, 77%, 75%)'},
+  {id:2,  color: 'black', backgroundColor: 'hsl(25, 88%, 74%)'},
+  {id:3,  color: 'black', backgroundColor: 'hsl(120, 87%, 78%)'},
+  {id:4,  color: 'black', backgroundColor: 'hsl(180, 85%, 70%)'},
+  {id:5,  color: 'black', backgroundColor: 'hsl(60, 80%, 50%)'},
+  {id:6,  color: 'black', backgroundColor: 'hsl(39, 90%, 45%)'},
+  {id:7,  color: 'black', backgroundColor: 'hsl(0, 71%, 69%)'},
+  {id:8,  color: 'black', backgroundColor: 'hsl(150, 88%, 44%)'},
+  {id:9,  color: 'black', backgroundColor: 'hsl(195, 53%, 79%)'},
+  {id:10, color: 'black', backgroundColor: 'hsl(302, 55%, 70%)'},
+  {id:11, color: 'black', backgroundColor: 'hsl(0, 29%, 72%)'},
+  {id:12, color: 'black', backgroundColor: 'hsl(20, 88%, 60%)'},
+  {id:13, color: 'black', backgroundColor: 'hsl(95, 76%, 56%)'},
+  {id:14, color: 'black', backgroundColor: 'hsl(49, 84%, 45%)'},
+  {id:15, color: 'black', backgroundColor: 'hsl(195, 100%, 60%)'},
+  {id:16, color: 'black', backgroundColor: 'hsl(115, 20%, 60%)'},
+  {id:17, color: 'black', backgroundColor: 'hsl(256, 62%, 76%)'},
+  {id:18, color: 'black', backgroundColor: 'hsl(300, 81%, 70%)'},
+  {id:19, color: 'black', backgroundColor: 'hsl(39, 91%, 57%)'},
+  {id:20, color: 'black', backgroundColor: 'hsl(0, 4%, 68%)'},
+  {id:21, color: 'white', backgroundColor: 'hsl(0, 68%, 42%)'},
+  {id:22, color: 'white', backgroundColor: 'hsl(120, 61%, 34%)'},
+  {id:23, color: 'white', backgroundColor: 'hsl(210, 62%, 54%)'},
+  {id:24, color: 'white', backgroundColor: 'hsl(271, 76%, 53%)'},
+  {id:25, color: 'white', backgroundColor: 'hsl(240, 92%, 59%)'},
+  {id:26, color: 'white', backgroundColor: 'hsl(60, 89%, 22%)'},
+  {id:27, color: 'white', backgroundColor: 'hsl(0, 77%, 60%)'},
+  {id:28, color: 'white', backgroundColor: 'hsl(25, 76%, 31%)'},
+  {id:29, color: 'white', backgroundColor: 'hsl(328, 66%, 35%)'},
+  {id:30, color: 'white', backgroundColor: 'hsl(249, 100%, 70%)'},
+  {id:31, color: 'white', backgroundColor: 'hsl(80, 65%, 35%)'},
+  {id:32, color: 'white', backgroundColor: 'hsl(180, 100%, 27%)'},
+  {id:33, color: 'white', backgroundColor: 'hsl(210, 13%, 50%)'},
+  {id:34, color: 'white', backgroundColor: 'hsl(338, 47%, 52%)'},
+  {id:35, color: 'white', backgroundColor: 'hsl(125, 73%, 20%)'},
+  {id:36, color: 'white', backgroundColor: 'hsl(18, 91%, 48%)'},
+  {id:37, color: 'white', backgroundColor: 'hsl(220, 89%, 28%)'},
+  {id:38, color: 'white', backgroundColor: 'hsl(312, 80%, 42%)'},
+  {id:39, color: 'black', backgroundColor: 'white'}
+];
