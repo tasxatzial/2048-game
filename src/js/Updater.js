e6aedd9fb4e92c02b2ad7882af6819e7643e960c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-07-24 20:59:55 +0300

    game version updater will now always set the current version

--- src/js/Updater.js
+++ src/js/Updater.js
@@ -2,12 +2,15 @@ export default class Updater {
   constructor() {}
 
   update() {
-    const game = JSON.parse(localStorage.getItem('game-2048'));
-    if (game && localStorage.getItem('game-2048-version') !== '2') {
-      this.updateToLatest(game);
-      localStorage.setItem('game-2048', JSON.stringify(game));
-      localStorage.setItem('game-2048-version', '2');
+    const version = localStorage.getItem('game-2048-version');
+    if (version === '2') {
+      return;
     }
+    const game = JSON.parse(localStorage.getItem('game-2048'));
+    if (game && this.updateToLatest(game)) {
+      localStorage.setItem('game-2048', JSON.stringify(game));
+    }
+    localStorage.setItem('game-2048-version', '2');
   }
 
   // will modify game
@@ -16,6 +19,9 @@ export default class Updater {
     for (let i = 0; i < gridArray.length; i++) {
       for (let j = 0; j < gridArray[0].length; j++) {
         const cell = gridArray[i][j];
+        if (cell.tiles) { // exists in v2+ games
+          return false;
+        }
         cell.tiles = [];
         if (cell.tile) {
           cell.tiles.push(cell.tile);
@@ -25,5 +31,6 @@ export default class Updater {
         delete cell.mergeTiles;
       }
     }
+    return true;
   }
 }
