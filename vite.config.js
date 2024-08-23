db4c03f4b0004215d4b74502500633ea798890e6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-08-24 02:25:24 +0300

    configure vite to also include colors.html as an entry point

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "src",
-  base: "/2048-game/",
+  root: 'src',
+  base: '/2048-game/',
   build: {
     emptyOutDir: true,
     outDir: '../dist',
     assetsInlineLimit: 0,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './src/index.html'),
+        colors: resolve(__dirname, './src/colors.html'),
+      },
+    },
   },
   test: {
     environment: 'jsdom',
     include: ['../tests/**/*.test.js'],
     coverage: {
       enabled: true,
-      reportsDirectory: "../coverage",
+      reportsDirectory: '../coverage',
       reporter: ['html']
     },
   }
